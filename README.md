177d059c08b26efcd7641f1cfdd6620ec67a68a4
Walison Coutinho <wallisoncouttinho@gmail.com>
2025-06-21 13:40:24 +0000

    new

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# GERENC_RESERVAS_HOTEL
+
+Equipe:
+[MATHEUS FOGAÇA]
+[GUILHERME BARBOSA ]
+[WALISON SILVA COUTINHO]
+[]
+[]
+
+Descrição do Projeto
+
+Este projeto é um sistema simples em C para administrar as reservas de um hotel. O sistema permite o controle de reservas de quartos, check-in e check-out de hóspedes e a visualização das reservas atuais.
+
+Funcionalidades Implementadas
+Reserva de Quartos: Os usuários podem fazer reservas de quartos especificando o número do quarto e o tempo que ficará reservado.
+Check-in: Registra a entrada de hóspedes no hotel, marcando o quarto como ocupado na data de check-in (OBS: O Check-in só funciona para o primeiro que reservou o quarto).
+Check-out: Registra a saída de hóspedes do hotel, marcando o quarto como disponível na data de check-out.
+Visualização de Reservas Atuais: Permite aos usuários visualizarem todas as reservas atuais, mostrando informações como número do quarto, nome de quem reservou e o tempo que ficará reservado.
+Instalação
+Clonando o repositório
+
+Com git bash
+git clone https://github.com/jaoangeloni/SISTEMA-RESERVAS-DE-HOTEL.git
+Caso não tenha a ferramenta, basta clicar no botão de download e descompactar o arquivo.
+Compilação do Código:
+
+Certifique-se de ter um compilador C instalado no seu sistema.
+Abra a pasta como um projeto do CLion ou com qualquer outra IDE que compile códigos em C.
+Compile e execute o código com a interface da IDE.
+Uso
+Menu Principal:
+
+Ao executar o programa, você será apresentado a um menu principal.
+Selecione as opções numeradas para acessar as funcionalidades desejadas.
+Reserva de Quartos, Check-in e Check-out:
+
+Siga as instruções na tela para inserir as informações necessárias.
+Visualização de Reservas Atuais:
+
+Escolha a opção no menu para visualizar as reservas atuais.
+Encerramento do Programa:
+
+Use a opção apropriada no menu para sair do programa.
+Exemplo de Uso
+Reservar Quarto:
+Escolha a opção 1 no menu, insira o número do quarto e quantidade de dias que será resevado.
+Check-in:
+Escolha a opção 2 no menu, insira o número do quarto e a data de check-in.
+Check-out:
+Escolha a opção 3 no menu, insira o número do quarto e a data de check-out.
+Visualizar Reservas Atuais:
+Escolha a opção 4 no menu para ver todas as reservas atuais.
---
+++ funcoes.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include "funcoes.h"
+
+//duas pessoas podem fazer a reserva em um mesmo quarto, porém não podem fazer checkin em um mesmo
+//concertar a questão do reservas.ocupado
+
+void fazerReserva(Reserva* reservas, int* totalReservas) {
+
+    if (*totalReservas < MAXIMODERESERVAS){
+
+        printf("Digite o numero do quarto: ");
+        scanf("%d", &reservas[*totalReservas].numeroQuarto);
+
+        printf("Digite o nome do hospede: ");
+        scanf("%s", reservas[*totalReservas].hospede);
+
+        reservas[*totalReservas].ocupado = 0;
+        printf("Digite a quantidade de dias da reserva: ");
+        scanf("%d", &reservas[*totalReservas].diasReservados);
+
+        (*totalReservas)++;
+        printf("Reserva realizada!\n");
+
+    } else {
+
+        printf("Numero máxima de reservas Atingida.\n");
+
+    }
+}
+
+
+
+void realizarCheckIn(Reserva* reservas, int totalReservas) {
+    int numeroQuarto;
+    printf("Digite o numero do quarto para check-in: ");
+    scanf("%d", &numeroQuarto);
+
+    for (int i = 0; i < totalReservas; i++) {
+        if (reservas[i].numeroQuarto == numeroQuarto) {
+            if (reservas[i].ocupado == 1) {
+                printf("Quarto ja ocupado. Nao foi possivel realizar o check-in.\n");
+            } else {
+                reservas[i].ocupado = 1;
+                printf("Check-in realizado com sucesso!\n");
+            }
+            return;
+        }
+    }
+
+    printf("Quarto nao encontrado.\n");
+}
+
+void realizarCheckOut(Reserva* reservas, int totalReservas) {
+    int numeroQuarto;
+    printf("Digite o numero do quarto para check-out: ");
+    scanf("%d", &numeroQuarto);
+
+    for (int i = 0; i < totalReservas; i++) {
+        if (reservas[i].numeroQuarto == numeroQuarto && reservas[i].ocupado) {
+            reservas[i].ocupado = 0;
+            printf("Check-out realizado com sucesso!\n");
+            return;
+        }
+    }
+
+    printf("Quarto nao encontrado ou nao ocupado.\n");
+}
+
+void visualizarReservas(Reserva* reservas, int totalReservas) {
+    printf("Quartos Reservados:\n");
+    for (int i = 0; i < totalReservas; i++) {
+        if (reservas[i].ocupado == 1) {
+            printf("Quarto %d: %s - %d dias - ocupado\n", reservas[i].numeroQuarto, reservas[i].hospede, reservas[i].diasReservados);
+        } else{
+            printf("Quarto %d: %s - %d dias\n", reservas[i].numeroQuarto, reservas[i].hospede, reservas[i].diasReservados);
+        }
+    }
+}
